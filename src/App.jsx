685b3a6caaa51157d64eaf0f88c4a6a2eb9755bd
Jasur-Rustamov <isakovamatlyuba1959@gmail.com>
2025-02-14 14:28:56 +0500

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+export const LanguageContext = createContext();
+
+
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const navigate = useNavigate();
+
+  const [language, setLanguage] = useState('uzbek')
+
 
   useEffect(() => {
     if (!token) {
@@ -16,13 +22,13 @@ function App() {
   }, [token, navigate]);
 
   return (
-    <div>
+    <LanguageContext.Provider value={{language, setLanguage}}>
       <Routes>
         <Route path="/" element={token ? <Home /> : <Login />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </div>
+    </LanguageContext.Provider>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useTranslation } from 'react-i18next';
+
 
 function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+  const {t, i18n} = useTranslation();
+  const [lang, setLang] = useState('en')
+
+  const toggleMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  useEffect(() =>{
+    i18n.changeLanguage(lang)
+  }, [lang])
+
+  
   return (
-    <div>
-      <h1>Hello WOrld!</h1>
+      <div
+      className={`h-screen flex flex-col items-center justify-center ${
+        darkMode ? 'bg-black text-white' : 'bg-white text-black'
+      }`}
+    >
+      <button
+        onClick={toggleMode}
+        className="cursor-pointer px-6 py-3 bg-blue-600 text-white rounded-xl hover:bg-blue-800"
+      >
+        dark/Light Mode
+      </button>
+
+      <h1>{t('hello')} {t('how')} {t('are')} {t('you')} ?</h1>
+
+      <select className="cursor-pointer px-6 py-3 bg-blue-600 text-white rounded-xl hover:bg-blue-800" value={lang} onChange={(e) => {setLang(e.target.value)}} >
+        <option>en</option>
+        <option>ru</option>
+      </select>
+
     </div>
+    
   );
 }
 
